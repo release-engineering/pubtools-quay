78f5be3a85af51731125f543bdecd0511d5978e4
Lubomir Gallovic <lgallovi@redhat.com>
2021-09-07 09:27:45 +0200

    Prepare release v0.8.0

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ if os.environ.get("READTHEDOCS", None):
 
 setup(
     name="pubtools-quay",
-    version="0.7.2",
+    version="0.8.0",
     description="Pubtools-quay",
     long_description=long_description,
     long_description_content_type='text/x-rst',
