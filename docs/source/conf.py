a4b7f19e29d5667941496a39622f710e43d46991
Jindrich Luza <jluza@redhat.com>
2022-10-24 17:29:44 +0200

    release v0.10.4 (#151)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ copyright = u"2021, Red Hat"
 author = u"Red Hat"
 
 # The short X.Y version
-version = u"0.10.3"
+version = u"0.10.4"
 # The full version, including alpha/beta/rc tags
-release = u"0.10.3"
+release = u"0.10.4"
 
 
 # -- General configuration ---------------------------------------------------
