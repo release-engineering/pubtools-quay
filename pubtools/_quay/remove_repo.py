6012c2dfaa026c3f81cd0f9201f4dce7c8ed3b5b
Jindrich Luza <jluza@redhat.com>
2024-02-21 09:30:25 +0100

    Fixed signing remove repo argument  (#226)
    * setup was environment variables before signing
    
    * Fixed remove repo task

--- pubtools/_quay/remove_repo.py
+++ pubtools/_quay/remove_repo.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import argparse
 from typing import Any
@@ -116,7 +117,7 @@ def remove_repositories(repositories: str, settings: dict[str, Any]) -> None:
 
     LOG.info("Removing repositories '{0}'".format(repositories))
     quay_api_client = QuayApiClient(settings["quay_api_token"])
-    quay_client = QuayClient(settings["quay_user"], settings["quay_password"])
+    quay_client = QuayClient(settings["quay_user"], settings["quay_password"], "quay.io")
     item_processor = item_processor_for_internal_data(
         quay_client, "quay.io", 5, settings["quay_org"]
     )
@@ -135,9 +136,14 @@ def remove_repositories(repositories: str, settings: dict[str, Any]) -> None:
             continue
         outdated_manifests.append((mad.digest, tag, repo))
 
+    signer_settings = {k: v for k, v in settings.items() if k not in ["quay_org"]}
+    signer_settings["quay_namespace"] = settings["quay_org"]
+    signer_settings["dest_quay_api_token"] = os.environ.get("QUAY_API_TOKEN")
+    signer_settings["quay_host"] = "quay.io"
+
     for n, signer in enumerate(signers):
         signercls = SIGNER_BY_LABEL[signer]
-        signer = signercls(config_file=signer_configs[0], settings=settings)
+        signer = signercls(config_file=signer_configs[0], settings=signer_settings)
         signer.remove_signatures(outdated_manifests, _exclude=[])
 
     for repository in parsed_repositories:
