2cd8e69a78fdeaff7b4cd492feb3848a08dd47dd
Jindrich Luza <jluza@redhat.com>
2024-05-21 11:36:39 +0200

    Release v0.22.0

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ if os.environ.get("READTHEDOCS", None):
 
 setup(
     name="pubtools-quay",
-    version="0.21.0",
+    version="0.22.0",
     description="Pubtools-quay",
     long_description=long_description,
     long_description_content_type="text/x-rst",
